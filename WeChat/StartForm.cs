25c94968c1892db58e930e5f8657fb11a7810c4c
MoYan <2607077992@qq.com>
2024-04-09 16:00:01 +0800

    添加项目文件。

--- WeChat/StartForm.cs
+++ WeChat/StartForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenWeChat;
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
@@ -9,35 +10,40 @@ namespace WeChat
 {
     public partial class StartForm : Form
     {
-        #region systemDLL
+        #region DLL引用
 
-        [DllImport("user32.dll")]
-        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        public void INIWrite(string key, string value, string section = "WeChat")
+        {
+            DLL.WriteINI(section, key, value, PATH);
+        }
 
-        [DllImport("user32.dll")]
-        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        public string INIRead(string key, string section = "WeChat")
+        {
+            System.Text.StringBuilder temp = new System.Text.StringBuilder(255);
+            DLL.GetINI(section, key, "", temp, 255, PATH);
+            return temp.ToString();
+        }
 
-        [DllImport("user32.dll")]
-        private static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
-
-        [DllImport("user32.dll")]
-        private static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
-
-        [DllImport("kernel32")]
-        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
-
-        [DllImport("kernel32")]
-        private static extern int GetPrivateProfileString(string section, string key, string def, System.Text.StringBuilder retVal, int size, string filePath);
-
-        #endregion systemDLL
+        #endregion DLL引用
 
         #region INIT
 
+        private SystemDLL DLL = new SystemDLL();
         private static string CmdPath = @"C:\Windows\System32\cmd.exe";
         private string PATH = string.Empty;
         private Thread thread;
 
         #endregion INIT
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams paras = base.CreateParams;
+                paras.ExStyle |= 0x02000000;
+                return paras;
+            }
+        }
 
         public StartForm()
         {
@@ -59,22 +65,6 @@ namespace WeChat
                 }
             }
         }
-
-        #region DLL引用
-
-        public void INIWrite(string key, string value, string section = "WeChat")
-        {
-            WritePrivateProfileString(section, key, value, PATH);
-        }
-
-        public string INIRead(string key, string section = "WeChat")
-        {
-            System.Text.StringBuilder temp = new System.Text.StringBuilder(255);
-            GetPrivateProfileString(section, key, "", temp, 255, PATH);
-            return temp.ToString();
-        }
-
-        #endregion
 
         /// <summary>
         /// start
@@ -105,7 +95,6 @@ namespace WeChat
                         }
 
                         StartWeChat(INIRead("OpenWeChatPath"), count);
-
                     }
                     catch (Exception)
                     {
@@ -117,7 +106,6 @@ namespace WeChat
             {
                 MessageBox.Show("还未设置启动环境");
             }
-
         }
 
         /// <summary>
@@ -136,48 +124,44 @@ namespace WeChat
                 });
             }
 
-            thread = new Thread(() =>
+            string cmd = "start \"\" ";
+            for (int i = 0; i < count; i++)
             {
-                string cmd = "start \"\" ";
-                for (int i = 0; i < count; i++)
+                cmd += $"\"{path}\"&";
+            }
+            cmd += "exit";
+
+            using (Process p = new Process())
+            {
+                p.StartInfo.FileName = CmdPath;
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+
+                p.StandardInput.WriteLine(cmd);
+                p.StandardInput.AutoFlush = true;
+
+                //string output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+                p.Close();
+            }
+
+            ShowLogin login = new ShowLogin();
+            if (login.ShowDialog() == DialogResult.Yes)
+            {
+                Process[] process = Process.GetProcessesByName("WeChat");
+                if (process.Length > 0)
                 {
-                    cmd += $"\"{path}\"&";
-                }
-                cmd += "exit";
-
-                using (Process p = new Process())
-                {
-                    p.StartInfo.FileName = CmdPath;
-                    p.StartInfo.UseShellExecute = false;
-                    p.StartInfo.RedirectStandardInput = true;
-                    p.StartInfo.RedirectStandardOutput = true;
-                    p.StartInfo.RedirectStandardError = true;
-                    p.StartInfo.CreateNoWindow = true;
-                    p.Start();
-
-                    p.StandardInput.WriteLine(cmd);
-                    p.StandardInput.AutoFlush = true;
-
-                    //string output = p.StandardOutput.ReadToEnd();
-                    p.WaitForExit();
-                    p.Close();
-                }
-
-                if (MessageBox.Show("请先全部登录\r\n完成后在点击\"是\"", "info", MessageBoxButtons.YesNo) == DialogResult.Yes)
-                {
-                    Process[] process = Process.GetProcessesByName("WeChat");
                     for (int i = 0; i < count; i++)
                     {
-                        Invoke(new MethodInvoker(delegate
-                        {
-                            SetParent(process[i].MainWindowHandle, tabControl1.TabPages[i].Handle);
-                            ShowWindow(process[i].MainWindowHandle, (int)ProcessWindowStyle.Maximized);
-                        }));
+                        DLL.SetParentWindow(process[i].MainWindowHandle, tabControl1.TabPages[i].Handle);
+                        DLL.SetShowWindow(process[i].MainWindowHandle, (int)ProcessWindowStyle.Maximized);
                     }
                 }
-            });
-            thread.IsBackground = true;
-            thread.Start();
+            }
         }
 
         /// <summary>
@@ -217,6 +201,105 @@ namespace WeChat
         private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             INIWrite("OpenCount", (toolStripComboBox1.SelectedIndex + 1).ToString());
+        }
+
+        private void 设置锁定密码ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowLock showLock = new ShowLock(Statc.setting);
+            if (showLock.ShowDialog() == DialogResult.OK)
+            {
+                INIWrite("Password", showLock.Tag.ToString());
+            }
+        }
+
+        private void 锁定ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(PATH))
+            {
+                if (INIRead("Password") != string.Empty)
+                {
+                    panel1.Visible = false;
+                    ShowLock showLock = new ShowLock(Statc.Lock, INIRead("Password"));
+                    if (showLock.ShowDialog() == DialogResult.OK)
+                    {
+                        panel1.Visible = true;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("还未设置锁定密码");
+                    设置锁定密码ToolStripMenuItem_Click(null, null);
+                }
+            }
+        }
+
+        private void StartForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        /// <summary>
+        /// 退出
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Dispose();
+                this.Close();
+                Environment.Exit(0);
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+
+    public class SystemDLL
+    {
+        #region systemDLL
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        private static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, System.Text.StringBuilder retVal, int size, string filePath);
+
+        #endregion systemDLL
+
+        public void SetParentWindow(IntPtr hwnd, IntPtr Parent)
+        {
+            SetParent(hwnd, Parent);
+        }
+
+        public void SetShowWindow(IntPtr hwnd, int nCmdShow)
+        {
+            ShowWindow(hwnd, nCmdShow);
+        }
+
+        public void WriteINI(string s, string key, string val, string file)
+        {
+            WritePrivateProfileString(s, key, val, file);
+        }
+
+        public void GetINI(string section, string key, string def, System.Text.StringBuilder retVal, int size, string filePath)
+        {
+            GetPrivateProfileString(section, key, def, retVal, size, filePath);
         }
     }
 }

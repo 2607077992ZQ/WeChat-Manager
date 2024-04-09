31f1d51912f5ed94f76f953fe091417e8fa78a7e
MoYan <2607077992@qq.com>
2024-04-09 16:00:01 +0800

    添加项目文件。

---
+++ WeChat/StartForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace WeChat
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.多开数量ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(834, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.多开数量ToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(44, 21);
+            this.toolStripMenuItem1.Text = "设置";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem2.Text = "路径设置";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // 多开数量ToolStripMenuItem
+            // 
+            this.多开数量ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem3,
+            this.toolStripComboBox1});
+            this.多开数量ToolStripMenuItem.Name = "多开数量ToolStripMenuItem";
+            this.多开数量ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.多开数量ToolStripMenuItem.Text = "多开数量";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(181, 22);
+            this.toolStripMenuItem3.Text = "启动";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox1.Text = "1";
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 25);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(834, 486);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(834, 511);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("宋体", 12F);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "StartForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem 多开数量ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+    }
+}
+
---
+++ WeChat/StartForm.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WeChat
+{
+    public partial class StartForm : Form
+    {
+        #region systemDLL
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        private static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, System.Text.StringBuilder retVal, int size, string filePath);
+
+        #endregion systemDLL
+
+        #region INIT
+
+        private static string CmdPath = @"C:\Windows\System32\cmd.exe";
+        private string PATH = string.Empty;
+        private Thread thread;
+
+        #endregion INIT
+
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PATH = Environment.CurrentDirectory + "\\wechatconfig.ini";
+            if (File.Exists(PATH))
+            {
+                try
+                {
+                    toolStripComboBox1.SelectedIndex = Convert.ToInt32(INIRead("OpenCount")) - 1;
+                }
+                catch (Exception)
+                {
+                    File.Delete(PATH);
+                }
+            }
+        }
+
+        #region DLL引用
+
+        public void INIWrite(string key, string value, string section = "WeChat")
+        {
+            WritePrivateProfileString(section, key, value, PATH);
+        }
+
+        public string INIRead(string key, string section = "WeChat")
+        {
+            System.Text.StringBuilder temp = new System.Text.StringBuilder(255);
+            GetPrivateProfileString(section, key, "", temp, 255, PATH);
+            return temp.ToString();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// start
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(PATH))
+            {
+                if (INIRead("OpenWeChatPath") != string.Empty && INIRead("OpenCount") != string.Empty)
+                {
+                    if (!INIRead("OpenWeChatPath").Contains("WeChat"))
+                    {
+                        toolStripMenuItem2_Click(null, null);
+                    }
+
+                    try
+                    {
+                        int count = 0;
+                        count = Convert.ToInt32(INIRead("OpenCount"));
+
+                        Process[] process = Process.GetProcessesByName("WeChat");
+                        foreach (var item in process)
+                        {
+                            //kill wechat
+                            item.Kill();
+                        }
+
+                        StartWeChat(INIRead("OpenWeChatPath"), count);
+
+                    }
+                    catch (Exception)
+                    {
+                        INIWrite("OpenCount", "1");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("还未设置启动环境");
+            }
+
+        }
+
+        /// <summary>
+        /// start
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="count"></param>
+        private void StartWeChat(string path, int count)
+        {
+            tabControl1.TabPages.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                tabControl1.TabPages.Add(new TabPage
+                {
+                    Text = $"账号{i + 1}"
+                });
+            }
+
+            thread = new Thread(() =>
+            {
+                string cmd = "start \"\" ";
+                for (int i = 0; i < count; i++)
+                {
+                    cmd += $"\"{path}\"&";
+                }
+                cmd += "exit";
+
+                using (Process p = new Process())
+                {
+                    p.StartInfo.FileName = CmdPath;
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardInput = true;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.RedirectStandardError = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.Start();
+
+                    p.StandardInput.WriteLine(cmd);
+                    p.StandardInput.AutoFlush = true;
+
+                    //string output = p.StandardOutput.ReadToEnd();
+                    p.WaitForExit();
+                    p.Close();
+                }
+
+                if (MessageBox.Show("请先全部登录\r\n完成后在点击\"是\"", "info", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Process[] process = Process.GetProcessesByName("WeChat");
+                    for (int i = 0; i < count; i++)
+                    {
+                        Invoke(new MethodInvoker(delegate
+                        {
+                            SetParent(process[i].MainWindowHandle, tabControl1.TabPages[i].Handle);
+                            ShowWindow(process[i].MainWindowHandle, (int)ProcessWindowStyle.Maximized);
+                        }));
+                    }
+                }
+            });
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        /// <summary>
+        /// wechat path
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Files (*.exe)|*.exe"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog.FileName;
+                bool flag = true;
+                if (File.Exists(PATH))
+                {
+                    flag = false;
+                }
+
+                if (path.Contains("WeChat"))
+                {
+                    INIWrite("OpenWeChatPath", path);
+                    if (flag) INIWrite("OpenCount", "1");
+                }
+            }
+        }
+
+        /// <summary>
+        /// count
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            INIWrite("OpenCount", (toolStripComboBox1.SelectedIndex + 1).ToString());
+        }
+    }
+}
